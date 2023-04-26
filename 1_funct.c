59df20e9d48553eeee5aa1d2eb1c7df10d3d0fe8
Manalhub <manalrahali24@gmail.com>
2023-04-25 18:55:41 -0700

    more functions

---
+++ 1_funct.c
@@ -0,0 +1,51 @@
+#include"main.h"
+
+/**
+ * printpercent - prints a percent sign to stdout
+ *
+ */
+int printpercent(va_list args)
+{
+	(void)(args);
+	_putchar('%');
+	return (1);
+}
+
+int printint(int n)
+{
+	if ((n / 10) >= 1)
+	{
+		printint(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+	return (1);
+}
+
+int printinteger(va_list args)
+{
+	int n = va_arg(args, int);
+	int i = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i++;
+		n = -n;
+	}
+
+	if ((n / 10) >= 1)
+	{
+		printint(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+
+	while (n != 0)
+	{
+		n /= 10;
+		i++;
+	}
+
+	return (i);
+}
