a35d7d194e6b73f36aefa2605433d27f60645f1c
Manalhub <manalrahali24@gmail.com>
2023-04-25 18:02:18 -0700

    the header file

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* C standard library headers*/
+#include <stdarg.h>
+#include <unistd.h>
+
+/* functions prototype */
+int _putchar(char z);
+int _strcmp(const char *s1, const char *s2);
+int printchar(va_list args);
+int printstring(va_list args);
+int printpercent(va_list args);
+int printint(int n);
+int printinteger(va_list args);
+int (*theprint(const char *format))(va_list);
+int _printf(const char *format, ...);
+
+typedef struct formats
+{
+        char *specf;
+        int (*function)(va_list);
+} format_t;
+
+#endif
