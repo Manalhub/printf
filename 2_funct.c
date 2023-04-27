6a13807b7bd75ab2cd4f863d533cb6afb9a1ecdf
samurai0lava <ouhsseineilyass@gmail.com>
2023-04-26 18:44:23 -0700

    betty style

--- 2_funct.c
+++ 2_funct.c
@@ -1,21 +1,35 @@
 #include"main.h"
 
+/**
+ * theprint - function that selects the correct function to perform the operation
+ * asked by the user.
+ * @format: operator passed as argument to the program.
+ *
+ * Return: a pointer to the function that corresponds to the operator given as a
+ * parameter. Example: if s is passed as a parameter, this function will return
+ * a pointer to the function print_s.
+ 
+*/
+
 int (*theprint(const char *format))(va_list)
 {
-	int i = 0;
-	format_t formats[] = {
-		{"c", printchar},
-		{"s", printstring},
-		{"%", printpercent},
-		{"d", printinteger},
-		{"i", printinteger},
-		{NULL, NULL}};
+        int i = 0;
 
-	for (; formats[i].specf != NULL; i++)
-	{
-		if (*format==*(formats[i].specf))
-			return formats[i].function;
-	}
+        format_t formats[] =
+        {
+                {"c", printchar},
+                {"s", printstring},
+                {"%", printpercent},
+                {"d", printinteger},
+                {"i", printinteger},
+                {NULL, NULL}
+        };
 
-	return (NULL);
+        for (; formats[i].specf != NULL; i++)
+        {
+                if (*format == *(formats[i].specf))
+                return formats[i].function;
+        }
+
+        return (NULL);
 }
