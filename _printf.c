61f549c083aca7b8c92770bd4513ea90c6f1264d
Manalhub <manalrahali24@gmail.com>
2023-04-28 15:24:16 -0700

    fix some betty style errors _printf.c

--- _printf.c
+++ _printf.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
- * _printf - prints formatted text to stdout
- * @format: the format string
+ * theprint - selects the function to use based on the given format specifier.
  *
- * Return: number of characters printed (excluding the null byte)
+ * @format: the format specifier character.
+ * @args: a va_list of arguments for the selected function.
+ *
+ * Return: a pointer to the function corresponding to the format specifier.
+ *         NULL if no matching function is found.
  */
 int _printf(const char *format, ...)
 {
